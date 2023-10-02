c97c855937940a4130e7cb198d6bc14813bd66e3
yuto-nakaie <yuto.nakaie@ivis.co.jp>
2023-10-02 17:19:06 +0900

    サンプルソースファイルを追加

---
+++ workspace/SpringBootAdminClient/src/main/java/com/example/SpringBootAdminClient/SpringBootAdminClient.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAdminClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClient.class, args);
+	}
+
+}
---
+++ workspace/SpringBootAdminClient/src/test/java/com/example/SpringBootAdminClient/SpringBootAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootAdminClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
